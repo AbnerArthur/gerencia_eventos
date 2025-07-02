2c35014121bf6dfd0e20d812eb4af14f8fac39bc
Arthur Corrêa <118570808+ArthurccSabatel@users.noreply.github.com>
2025-07-02 17:06:05 -0400

    Atualizando

--- dados_eventos.py
+++ dados_eventos.py
@@ -2,21 +2,21 @@ eventos = [
     {
         'codigo': 'E001',
         'nome': 'Encontro de Programadores',
-        'data': '2025-07-01',
+        'data': '2025/07/01',
         'tema': 'Python e boas práticas',
         'participantes': ['P001', 'P002']
     },
     {
         'codigo': 'E002',
         'nome': 'Workshop Front-End',
-        'data': '2025-07-10',
+        'data': '2025/07/10',
         'tema': 'HTML, CSS e JavaScript',
         'participantes': ['P002']
     },
     {
         'codigo': 'E003',
         'nome': 'Hackathon Tech',
-        'data': '2025-07-20',
+        'data': '2025/07/20',
         'tema': 'Inovação e Prototipagem',
         'participantes': ['P003']
     }
