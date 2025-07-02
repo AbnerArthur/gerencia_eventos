9adaa73f919213ae07c8c485622aa4e0de27e0a6
Arthur Corrêa <118570808+ArthurccSabatel@users.noreply.github.com>
2025-07-02 13:51:13 -0400

    primeiro commit

---
+++ dados_eventos.py
@@ -0,0 +1,23 @@
+eventos = [
+    {
+        'codigo': 'E001',
+        'nome': 'Encontro Python Avançado',
+        'data': '2025-07-01',
+        'tema': 'Python e boas práticas',
+        'participantes': ['P001', 'P002']  # Abner, Pedro Lucas
+    },
+    {
+        'codigo': 'E002',
+        'nome': 'Segurança na Web',
+        'data': '2025-07-10',
+        'tema': 'Segurança da Informação',
+        'participantes': ['P004']  # Geraldo
+    },
+    {
+        'codigo': 'E003',
+        'nome': 'Infraestrutura e Cloud',
+        'data': '2025-07-20',
+        'tema': 'DevOps e Cloud',
+        'participantes': ['P003', 'P001']  # Eduardo, Abner
+    }
+]
---
+++ dados_participantes.py
@@ -0,0 +1,26 @@
+participantes = [
+    {
+        'codigo': 'P001',
+        'nome': 'Abner',
+        'email': 'abner@email.com',
+        'preferencias': ['Python', 'IA']
+    },
+    {
+        'codigo': 'P002',
+        'nome': 'Pedro Lucas',
+        'email': 'pedrolucas@email.com',
+        'preferencias': ['Banco de Dados', 'Back-End']
+    },
+    {
+        'codigo': 'P003',
+        'nome': 'Eduardo',
+        'email': 'eduardo@email.com',
+        'preferencias': ['DevOps', 'Cloud']
+    },
+    {
+        'codigo': 'P004',
+        'nome': 'Geraldo',
+        'email': 'geraldo@email.com',
+        'preferencias': ['Segurança da Informação', 'Redes']
+    }
+]
